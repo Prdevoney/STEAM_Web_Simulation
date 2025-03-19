088c2818bfd1e254685ad20bd86ab116578a9034
Prdevoney <prdevo@gmail.com>
2025-03-18 22:32:10 -0400

    Bypassed the type checking on the gzweb import.

--- steam-web-simulation/src/App.tsx
+++ steam-web-simulation/src/App.tsx
@@ -1,21 +1,15 @@
 import React from 'react';
+import SceneManagerComponent from  './SceneManager';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload. why is this not working!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React and then some! Whi is this not shello aving? 
-        </a>
+        <h1>Scene Manager</h1>
       </header>
+      <main>
+        <SceneManagerComponent />
+      </main>
     </div>
   );
 }
